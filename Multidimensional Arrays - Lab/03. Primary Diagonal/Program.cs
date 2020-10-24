31cdd6756d853856b4a3bb4ecd4b7e420e86035f
Antonov-S <anto1nov1@gmail.com>
2020-10-24 11:59:22 +0300

    Multidimensional Arrays - Lab

---
+++ Multidimensional Arrays - Lab/03. Primary Diagonal/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine().Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            printPrincipalDiagonal(matrix, n);
+        }
+
+        static void printPrincipalDiagonal(int[,] mat, int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+
+                    // Condition for principal diagonal
+                    if (i == j)
+                    {
+                        sum += mat[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+            
+        }
+
+        
+    }
+}
---
+++ Multidimensional Arrays - Lab/4. Symbol in Matrix/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._Symbol_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+
+            char symbolToFind = char.Parse(Console.ReadLine());
+            bool isFound = false;
+            int rowFound = 0;
+            int colFound = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    char currentSymbol = matrix[row, col];
+                    if (currentSymbol == symbolToFind)
+                    {
+                        isFound = true;
+                        rowFound = row;
+                        colFound = col;
+                        break;
+                    }
+                    
+                }
+            }
+
+            if (isFound)
+            {
+                Console.WriteLine($"({rowFound}, {colFound})");
+            }
+            else
+            {
+                Console.WriteLine($"{symbolToFind} does not occur in the matrix");
+            }
+
+
+        }
+    }
+}
