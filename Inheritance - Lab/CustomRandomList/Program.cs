d922d649502391c845c85d339b5d4eb888eab4bd
Antonov-S <anto1nov1@gmail.com>
2020-10-29 16:26:17 +0200

    Inheritance - Lab (last 2 problems)

---
+++ Inheritance - Lab/CustomRandomList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("Gosho");
+            list.Add("Pesho");
+            list.Add("Sasho");
+            list.Add("Gero");
+            list.Add("Mero");
+
+            Console.WriteLine(list.Count);
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.Count);
+
+        }
+    }
+}
