f937706b3003b9104ba6e919ae92936b15333f25
Antonov-S <anto1nov1@gmail.com>
2020-11-07 00:49:10 +0200

    Polymorphism - Lab

---
+++ Polymorphism - Lab/Shapes/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape sh = new Rectangle(7, 8);
+            Console.WriteLine(sh.Draw());
+            Console.WriteLine(sh.CalculateArea());
+            Console.WriteLine(sh.CalculatePerimeter());
+        }
+    }
+}
