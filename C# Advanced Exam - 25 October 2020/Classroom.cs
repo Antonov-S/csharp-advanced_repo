9c87f6031d0084d115670e66e99fe7ed245cc76d
Antonov-S <anto1nov1@gmail.com>
2020-12-15 22:16:08 +0200

    C# Advanced Exam - 25 October 2020
    Exam preparation

---
+++ C# Advanced Exam - 25 October 2020/Classroom.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Classroom;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+        private int capacity;
+        private int count;
+
+        public Classroom(int capacity)
+        {
+            this.students = new List<Student>(capacity);
+            this.Capacity = capacity;
+        }
+
+        public int Capacity { get; set; }
+        public int Count
+        {
+            get
+            {
+                return students.Count;
+            }
+        }
+
+        public string RegisterStudent(Student student)
+        {
+            var result = "No seats in the classroom";
+
+            if (this.Capacity > this.Count)
+            {
+                result = $"Added student {student.FirstName} {student.LastName}";
+                this.students.Add(student);
+            }
+
+            return result;
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            var result = "Student not found";
+
+            Student student = this.students
+                .FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+
+            if (student != null)
+            {
+                this.students.Remove(student);
+                result = $"Dismissed student {student.FirstName} {student.LastName}";
+            }
+
+            return result;
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+
+            var sb = new StringBuilder();
+            var list = this.students.Where(x => x.Subject == subject).ToList();
+
+            if (!list.Any())
+            {
+                return "No students enrolled for the subject";
+            }
+
+            sb.AppendLine($"Subject: {subject}");
+            sb.AppendLine("Students:");
+            foreach (var item in list)
+            {
+                sb.AppendLine($"{item.FirstName} {item.LastName}");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public int GetStudentsCount()
+        {
+            return this.Count;
+        }
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            return
+                this.students
+                .FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+        }
+
+
+
+
+    }
+}
---
+++ C# Advanced Exam - 25 October 2020/Regular Exam 25_10_2020/01. Scheduling/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input_tasks = Console.ReadLine();
+            string input_threads = Console.ReadLine();
+            int taskToKill = int.Parse(Console.ReadLine());
+
+            int[] tasks = input_tasks.Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] threads = input_threads.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stackTasks = new Stack<int>(tasks);
+            Queue<int> queueThreads = new Queue<int>(threads);
+
+            while (queueThreads.Any() && stackTasks.Any())
+            {
+                int currentThread = queueThreads.Peek();
+                int currentTask = stackTasks.Peek();
+
+                if (currentTask == taskToKill)
+                {
+                    Console.WriteLine($"Thread with value {currentThread} killed task {taskToKill}");
+                    Console.WriteLine(string.Join(" ", queueThreads));
+                    break;
+                }
+
+                if (currentThread >= currentTask)
+                {
+                    stackTasks.Pop();
+                    queueThreads.Dequeue();
+                }
+                else if (currentThread < currentTask)
+                {
+                    queueThreads.Dequeue();
+                }
+
+
+            }
+
+
+
+
+        }
+    }
+}
