b27eb6f681738c4870f43f7ff8732fa987240144
Antonov-S <anto1nov1@gmail.com>
2020-12-16 11:56:31 +0200

    C# Advanced Retake Exam - 19 August 2020

---
+++ C# Advanced Retake Exam - 19 August 2020/01. Flower Wreaths/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Flower_Wreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] roses = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] lilies = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> liliesStack = new Stack<int>(lilies);
+            Queue<int> rosesQueue = new Queue<int>(roses);
+
+            int wreathsCount = 0;
+            int storedFlowers = 0;
+
+            while (liliesStack.Any() && rosesQueue.Any())
+            {
+                int currentLilie = liliesStack.Peek();
+                int currentRoses = rosesQueue.Peek();
+                int sumOfcurrentFlowers = currentLilie + currentRoses;
+
+                if (sumOfcurrentFlowers == 15)
+                {
+                    wreathsCount++;
+                    liliesStack.Pop();
+                    rosesQueue.Dequeue();
+
+                }
+                else if (sumOfcurrentFlowers >= 15)
+                {
+                    liliesStack.Push(liliesStack.Pop() - 2);
+                }
+                else
+                {
+                    storedFlowers += liliesStack.Pop() + rosesQueue.Dequeue();
+                }
+            }
+
+            wreathsCount += storedFlowers / 15;
+
+            Console.WriteLine(wreathsCount >= 5 ?
+                $"You made it, you are going to the competition with {wreathsCount} wreaths!" :
+                $"You didn't make it, you need {5 - wreathsCount} wreaths more!");
+
+        }
+    }
+}
---
+++ C# Advanced Retake Exam - 19 August 2020/VetClinic/Clinic.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> petsList;
+        private int capacity;
+
+        public Clinic(int capacity)
+        {
+            this.petsList = new List<Pet>(capacity);
+            this.Capacity = capacity;
+        }
+
+        public int Capacity { get; set; }
+
+        public void Add(Pet pet)
+        {
+            if (this.Count < this.Capacity)
+            {
+                this.petsList.Add(pet);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Pet pet = this.petsList.FirstOrDefault(x => x.Name == name);
+
+            if (pet != null)
+            {
+                this.petsList.Remove(pet);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            Pet pet = this.petsList.FirstOrDefault(x => x.Name == name && x.Owner == owner);
+            return pet;
+        }
+
+        public Pet GetOldestPet()
+        {
+            Pet pet = this.petsList.OrderByDescending(x => x.Age)
+                .FirstOrDefault();
+
+            return pet;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.petsList.Count;
+            }
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+
+            foreach (var pet in petsList)
+            {
+                sb.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
